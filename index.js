8428e09d999e7cd081962e22b9eedf63df1945f7
Benxamin <bertrand.design@gmail.com>
2014-05-01 16:58:43 -0600

    Adds baseuri() and component reqs.

---
+++ index.js
@@ -0,0 +1,31 @@
+/**
+ * Expose baseuri().
+ */
+
+module.exports = baseuri;
+
+
+/**
+ * Return the base URI of the document.
+ *
+ * @return {String} uniform resource identifier
+ */
+function baseuri() {
+  
+  // Check expected property.
+  return document.baseURL || (function () {
+    
+    // Should be exactly one tag: 
+    //   https://developer.mozilla.org/en-US/docs/Web/HTML/Element/base
+    var base = document.getElementsByTagName('base');
+    
+    // Return location from BASE tag.
+    if (base.length > 0) {
+        return base[0].href;
+    }
+    
+    // Else use implementation of documentURI: 
+    //   http://www.w3.org/TR/DOM-Level-3-Core/core.html#Node3-baseURI
+    return document.URL;
+  })();
+}
